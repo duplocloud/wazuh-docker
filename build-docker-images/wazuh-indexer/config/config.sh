5ed8d38034623185bf5bf72ee1d9f8c1cbf3e259
arun hundaragi <arunhundaragi@aruns-MacBook-Pro.local>
2025-02-14 16:09:06 +0530

    internal user passing as a environment variables

--- build-docker-images/wazuh-indexer/config/config.sh
+++ build-docker-images/wazuh-indexer/config/config.sh
@@ -1,5 +1,10 @@
+#!/usr/bin/env bash
 # Wazuh Docker Copyright (C) 2017, Wazuh Inc. (License GPLv2)
-# This has to be exported to make some magic below work.
+# This script prepares the Wazuh indexer package and configuration files.
+# NOTE: The internal_users.yml file now uses placeholder values (e.g., "changeme")
+# for password hashes. At container startup, the entrypoint.sh will generate bcrypt hashes
+# from runtime-supplied plain passwords (via environment variables) and update this file accordingly.
+
 export DH_OPTIONS
 
 export NAME=wazuh-indexer
@@ -21,7 +26,7 @@ export REPO_DIR=/unattended_installer
 
 rm -rf ${INSTALLATION_DIR}/
 
-## variables
+## Variables to determine repository
 REPOSITORY="packages.wazuh.com/4.x"
 WAZUH_CURRENT_VERSION=$(curl --silent https://api.github.com/repos/wazuh/wazuh/releases/latest | grep '\"tag_name\":' | sed -E 's/.*\"([^\"]+)\".*/\1/' | cut -c 2-)
 MAJOR_BUILD=$(echo $WAZUH_VERSION | cut -d. -f1)
@@ -31,7 +36,7 @@ MAJOR_CURRENT=$(echo $WAZUH_CURRENT_VERSION | cut -d. -f1)
 MID_CURRENT=$(echo $WAZUH_CURRENT_VERSION | cut -d. -f2)
 MINOR_CURRENT=$(echo $WAZUH_CURRENT_VERSION | cut -d. -f3)
 
-## check version to use the correct repository
+## Check version to select the appropriate repository
 if [ "$MAJOR_BUILD" -gt "$MAJOR_CURRENT" ]; then
   REPOSITORY="packages-dev.wazuh.com/pre-release"
 elif [ "$MAJOR_BUILD" -eq "$MAJOR_CURRENT" ]; then
@@ -44,23 +49,20 @@ elif [ "$MAJOR_BUILD" -eq "$MAJOR_CURRENT" ]; then
   fi
 fi
 
-
 curl -o ${INDEXER_FILE} https://${REPOSITORY}/stack/indexer/${BASE_FILE}
 tar -xf ${INDEXER_FILE}
 
 ## TOOLS
 
-## Variables
 CERT_TOOL=wazuh-certs-tool.sh
 PASSWORD_TOOL=wazuh-passwords-tool.sh
 PACKAGES_URL=https://packages.wazuh.com/4.7/
 PACKAGES_DEV_URL=https://packages-dev.wazuh.com/4.7/
 
 ## Check if the cert tool exists in S3 buckets
-CERT_TOOL_PACKAGES=$(curl --silent -I $PACKAGES_URL$CERT_TOOL | grep -E "^HTTP" | awk  '{print $2}')
-CERT_TOOL_PACKAGES_DEV=$(curl --silent -I $PACKAGES_DEV_URL$CERT_TOOL | grep -E "^HTTP" | awk  '{print $2}')
+CERT_TOOL_PACKAGES=$(curl --silent -I $PACKAGES_URL$CERT_TOOL | grep -E "^HTTP" | awk '{print $2}')
+CERT_TOOL_PACKAGES_DEV=$(curl --silent -I $PACKAGES_DEV_URL$CERT_TOOL | grep -E "^HTTP" | awk '{print $2}')
 
-## If cert tool exists in some bucket, download it, if not exit 1
 if [ "$CERT_TOOL_PACKAGES" = "200" ]; then
   curl -o $CERT_TOOL $PACKAGES_URL$CERT_TOOL
   echo "Cert tool exists in Packages bucket"
@@ -72,12 +74,10 @@ else
   exit 1
 fi
 
-
 ## Check if the password tool exists in S3 buckets
-PASSWORD_TOOL_PACKAGES=$(curl --silent -I $PACKAGES_URL$PASSWORD_TOOL | grep -E "^HTTP" | awk  '{print $2}')
-PASSWORD_TOOL_PACKAGES_DEV=$(curl --silent -I $PACKAGES_DEV_URL$PASSWORD_TOOL | grep -E "^HTTP" | awk  '{print $2}')
+PASSWORD_TOOL_PACKAGES=$(curl --silent -I $PACKAGES_URL$PASSWORD_TOOL | grep -E "^HTTP" | awk '{print $2}')
+PASSWORD_TOOL_PACKAGES_DEV=$(curl --silent -I $PACKAGES_DEV_URL$PASSWORD_TOOL | grep -E "^HTTP" | awk '{print $2}')
 
-## If password tool exists in some bucket, download it, if not exit 1
 if [ "$PASSWORD_TOOL_PACKAGES" = "200" ]; then
   curl -o $PASSWORD_TOOL $PACKAGES_URL$PASSWORD_TOOL
   echo "Password tool exists in Packages bucket"
@@ -91,7 +91,7 @@ fi
 
 chmod 755 $CERT_TOOL && bash /$CERT_TOOL -A
 
-# copy to target
+# Create target directory structure
 mkdir -p ${TARGET_DIR}${INSTALLATION_DIR}
 mkdir -p ${TARGET_DIR}${INSTALLATION_DIR}/opensearch-security/
 mkdir -p ${TARGET_DIR}${CONFIG_DIR}
@@ -103,6 +103,7 @@ mkdir -p ${TARGET_DIR}/usr/lib/tmpfiles.d
 mkdir -p ${TARGET_DIR}/usr/lib/sysctl.d
 mkdir -p ${TARGET_DIR}/usr/lib/systemd/system
 mkdir -p ${TARGET_DIR}${CONFIG_DIR}/certs
+
 # Move configuration files for wazuh-indexer
 mv -f ${BASE_DIR}/etc/init.d/${NAME} ${TARGET_DIR}/etc/init.d/${NAME}
 mv -f ${BASE_DIR}/etc/wazuh-indexer/* ${TARGET_DIR}${CONFIG_DIR}
@@ -112,16 +113,21 @@ mv -f ${BASE_DIR}/usr/lib/sysctl.d/* ${TARGET_DIR}/usr/lib/sysctl.d/
 mv -f ${BASE_DIR}/usr/lib/systemd/system/* ${TARGET_DIR}/usr/lib/systemd/system/
 rm -rf ${BASE_DIR}/etc
 rm -rf ${BASE_DIR}/usr
+
 # Copy installation files to final location
 cp -pr ${BASE_DIR}/* ${TARGET_DIR}${INSTALLATION_DIR}
+
 # Copy the security tools
 cp /$CERT_TOOL ${TARGET_DIR}${INSTALLATION_DIR}/plugins/opensearch-security/tools/
 cp /$PASSWORD_TOOL ${TARGET_DIR}${INSTALLATION_DIR}/plugins/opensearch-security/tools/
-# Copy Wazuh's config files for the security plugin
+
+# Copy Wazuh's config files for the security plugin.
+# The internal_users.yml file (with placeholder hashes) will be updated at container runtime.
 cp -pr /roles_mapping.yml ${TARGET_DIR}${INSTALLATION_DIR}/opensearch-security/
 cp -pr /roles.yml ${TARGET_DIR}${INSTALLATION_DIR}/opensearch-security/
 cp -pr /internal_users.yml ${TARGET_DIR}${INSTALLATION_DIR}/opensearch-security/
 cp -pr /opensearch.yml ${TARGET_DIR}${CONFIG_DIR}
+
 # Copy Wazuh indexer's certificates
 cp -pr /wazuh-certificates/demo.indexer.pem ${TARGET_DIR}${CONFIG_DIR}/certs/indexer.pem
 cp -pr /wazuh-certificates/demo.indexer-key.pem ${TARGET_DIR}${CONFIG_DIR}/certs/indexer-key.pem
@@ -134,7 +140,6 @@ cp -pr /wazuh-certificates/admin-key.pem ${TARGET_DIR}${CONFIG_DIR}/certs/admin-
 sed '/-Xms/d' -i ${TARGET_DIR}${CONFIG_DIR}/jvm.options
 sed '/-Xmx/d' -i ${TARGET_DIR}${CONFIG_DIR}/jvm.options
 sed -i 's/-Djava.security.policy=file:\/\/\/etc\/wazuh-indexer\/opensearch-performance-analyzer\/opensearch_security.policy/-Djava.security.policy=file:\/\/\/usr\/share\/wazuh-indexer\/opensearch-performance-analyzer\/opensearch_security.policy/g' ${TARGET_DIR}${CONFIG_DIR}/jvm.options
-
 
 chmod -R 500 ${TARGET_DIR}${CONFIG_DIR}/certs
 chmod -R 400 ${TARGET_DIR}${CONFIG_DIR}/certs/*
--- build-docker-images/wazuh-indexer/config/entrypoint.sh
+++ build-docker-images/wazuh-indexer/config/entrypoint.sh
@@ -15,60 +15,93 @@ export KEY="${OPENSEARCH_PATH_CONF}/certs/admin-key.pem"
 
 run_as_other_user_if_needed() {
   if [[ "$(id -u)" == "0" ]]; then
-    # If running as root, drop to specified UID and run command
+    # If running as root, drop to UID 1000 and execute the command.
     exec chroot --userspec=1000:0 / "${@}"
   else
-    # Either we are running in Openshift with random uid and are a member of the root group
-    # or with a custom --user
     exec "${@}"
   fi
 }
 
-# Allow user specify custom CMD, maybe bin/opensearch itself
-# for example to directly specify `-E` style parameters for opensearch on k8s
-# or simply to run /bin/bash to check the image
+# Allow environment variables to be set via _FILE variables.
+# (This is also sourced in opensearch-env and is idempotent.)
+source /usr/share/wazuh-indexer/bin/opensearch-env-from-file
+
+# === New: Parameterize internal users passwords ===
+# The internal_users.yml file (used by the security plugin) is expected at:
+INTERNAL_USERS_FILE="${OPENSEARCH_PATH_CONF}/opensearch-security/internal_users.yml"
+
+if [ -f "$INTERNAL_USERS_FILE" ]; then
+  # Verify that htpasswd is available for generating bcrypt hashes.
+  if ! command -v htpasswd >/dev/null 2>&1; then
+    echo "Error: htpasswd command not found. Please ensure apache2-utils is installed." >&2
+    exit 1
+  fi
+
+#   update_user_password() {
+#   local user="$1"
+#   # Construct environment variable name (e.g., admin -> ADMIN_PASSWORD)
+#   local env_var_name
+#   env_var_name="$(echo "$user" | tr '[:lower:]' '[:upper:]')_PASSWORD"
+#   local plain_pass="${!env_var_name}"
+#   if [ -n "$plain_pass" ]; then
+#     # Generate a bcrypt hash using htpasswd.
+#     local hash_output
+#     hash_output=$(htpasswd -nbB "$user" "$plain_pass")
+#     # The output format is: username:hash; extract the hash.
+#     local hashed
+#     hashed=$(echo "$hash_output" | cut -d: -f2)
+#     echo "Updating password for user '$user'."
+#     # Updated sed command: use '#' as delimiter instead of '/'
+#     sed -i "/^$user:/,/^[^[:space:]]/ s#^\(\s*hash:\s*\).*#\1\"$hashed\"#" "$INTERNAL_USERS_FILE"
+#   fi
+# }
+
+update_user_password() {
+  local user="$1"
+  # Construct environment variable name (e.g., admin -> ADMIN_PASSWORD)
+  local env_var_name
+  env_var_name="$(echo "$user" | tr '[:lower:]' '[:upper:]')_PASSWORD"
+  local plain_pass="${!env_var_name}"
+  if [ -n "$plain_pass" ]; then
+    # Generate a bcrypt hash with cost 12 using htpasswd.
+    local hash_output
+    hash_output=$(htpasswd -nbB -C 12 "$user" "$plain_pass")
+    # Extract the hash portion (the output is: username:hash)
+    local hashed
+    hashed=$(echo "$hash_output" | cut -d: -f2)
+    echo "Updating password for user '$user'."
+    # Use '#' as delimiter to avoid conflicts with slashes in the hash.
+    sed -i "/^$user:/,/^[^[:space:]]/ s#^\(\s*hash:\s*\).*#\1\"$hashed\"#" "$INTERNAL_USERS_FILE"
+  fi
+}
+
+
+  for user in admin kibanaserver kibanaro logstash readall snapshotrestore wazuh_admin wazuh_user; do
+    update_user_password "$user"
+  done
+fi
+# === End password parameterization ===
+
+# Allow custom CMD parameters.
 if [[ "$1" != "opensearchwrapper" ]]; then
   if [[ "$(id -u)" == "0" && $(basename "$1") == "opensearch" ]]; then
-    # Rewrite CMD args to replace $1 with `opensearch` explicitly,
-    # Without this, user could specify `opensearch -E x.y=z` but
-    # `bin/opensearch -E x.y=z` would not work.
     set -- "opensearch" "${@:2}"
-    # Use chroot to switch to UID 1000 / GID 0
     exec chroot --userspec=1000:0 / "$@"
   else
-    # User probably wants to run something else, like /bin/bash, with another uid forced (Openshift?)
     exec "$@"
   fi
 fi
 
-# Allow environment variables to be set by creating a file with the
-# contents, and setting an environment variable with the suffix _FILE to
-# point to it. This can be used to provide secrets to a container, without
-# the values being specified explicitly when running the container.
-#
-# This is also sourced in opensearch-env, and is only needed here
-# as well because we use INDEXER_PASSWORD below. Sourcing this script
-# is idempotent.
-source /usr/share/wazuh-indexer/bin/opensearch-env-from-file
-
 if [[ -f bin/opensearch-users ]]; then
-  # Check for the INDEXER_PASSWORD environment variable to set the
-  # bootstrap password for Security.
-  #
-  # This is only required for the first node in a cluster with Security
-  # enabled, but we have no way of knowing which node we are yet. We'll just
-  # honor the variable if it's present.
+  # If INDEXER_PASSWORD is provided, set the bootstrap password in the keystore.
   if [[ -n "$INDEXER_PASSWORD" ]]; then
     [[ -f /usr/share/wazuh-indexer/opensearch.keystore ]] || (run_as_other_user_if_needed opensearch-keystore create)
     if ! (run_as_other_user_if_needed opensearch-keystore has-passwd --silent) ; then
-      # keystore is unencrypted
       if ! (run_as_other_user_if_needed opensearch-keystore list | grep -q '^bootstrap.password$'); then
         (run_as_other_user_if_needed echo "$INDEXER_PASSWORD" | opensearch-keystore add -x 'bootstrap.password')
       fi
     else
-      # keystore requires password
-      if ! (run_as_other_user_if_needed echo "$KEYSTORE_PASSWORD" \
-          | opensearch-keystore list | grep -q '^bootstrap.password$') ; then
+      if ! (run_as_other_user_if_needed echo "$KEYSTORE_PASSWORD" | opensearch-keystore list | grep -q '^bootstrap.password$') ; then
         COMMANDS="$(printf "%s\n%s" "$KEYSTORE_PASSWORD" "$INDEXER_PASSWORD")"
         (run_as_other_user_if_needed echo "$COMMANDS" | opensearch-keystore add -x 'bootstrap.password')
       fi
@@ -77,17 +110,15 @@ if [[ -f bin/opensearch-users ]]; then
 fi
 
 if [[ "$(id -u)" == "0" ]]; then
-  # If requested and running as root, mutate the ownership of bind-mounts
   if [[ -n "$TAKE_FILE_OWNERSHIP" ]]; then
     chown -R 1000:0 /usr/share/wazuh-indexer/{data,logs}
   fi
 fi
 
-
-#if [[ "$DISCOVERY" == "single-node" ]] && [[ ! -f "/var/lib/wazuh-indexer/.flag" ]]; then
-  # run securityadmin.sh for single node with CACERT, CERT and KEY parameter
-#  nohup /securityadmin.sh &
-#  touch "/var/lib/wazuh-indexer/.flag"
-#fi
+# Optionally, you could uncomment the following block to run securityadmin.sh for single-node setups.
+# if [[ "$DISCOVERY" == "single-node" ]] && [[ ! -f "/var/lib/wazuh-indexer/.flag" ]]; then
+#   nohup /securityadmin.sh &
+#   touch "/var/lib/wazuh-indexer/.flag"
+# fi
 
 run_as_other_user_if_needed /usr/share/wazuh-indexer/bin/opensearch <<<"$KEYSTORE_PASSWORD"
