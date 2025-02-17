a743d8ceb2ec7f2fe77530851f7c02c876402014
arun hundaragi <arunhundaragi@aruns-MacBook-Pro.local>
2025-02-17 16:51:19 +0530

    added azure snapshot and teams notification

--- build-docker-images/wazuh-indexer/config/entrypoint.sh
+++ build-docker-images/wazuh-indexer/config/entrypoint.sh
@@ -10,8 +10,8 @@ export OPENSEARCH_PATH_CONF=${INSTALLATION_DIR}
 export JAVA_HOME=${INSTALLATION_DIR}/jdk
 
 # Read discovery.type from opensearch.yml
-export DISCOVERY=$(grep -oP "(?<=discovery.type: ).*" "${OPENSEARCH_PATH_CONF}/opensearch.yml")
-export CACERT=$(grep -oP "(?<=plugins.security.ssl.transport.pemtrustedcas_filepath: ).*" "${OPENSEARCH_PATH_CONF}/opensearch.yml")
+export DISCOVERY=$(grep -oP "(?<=discovery.type: ).*" ${OPENSEARCH_PATH_CONF}/opensearch.yml)
+export CACERT=$(grep -oP "(?<=plugins.security.ssl.transport.pemtrustedcas_filepath: ).*" ${OPENSEARCH_PATH_CONF}/opensearch.yml)
 export CERT="${OPENSEARCH_PATH_CONF}/certs/admin.pem"
 export KEY="${OPENSEARCH_PATH_CONF}/certs/admin-key.pem"
 
@@ -45,19 +45,19 @@ source /usr/share/wazuh-indexer/bin/opensearch-env-from-file
 # ------------------------------------------------------------------------------
 if [[ -f bin/opensearch-users ]]; then
   # Check for the INDEXER_PASSWORD environment variable to set the bootstrap password for Security.
-  if [[ -n "$INDEXER_PASSWORD" ]]; then
-    [[ -f /usr/share/wazuh-indexer/opensearch.keystore ]] || (run_as_other_user_if_needed /usr/share/wazuh-indexer/bin/opensearch-keystore create)
-    if ! (run_as_other_user_if_needed /usr/share/wazuh-indexer/bin/opensearch-keystore has-passwd --silent) ; then
-      if ! (run_as_other_user_if_needed /usr/share/wazuh-indexer/bin/opensearch-keystore list | grep -q '^bootstrap.password$'); then
-       (run_as_other_user_if_needed echo "$INDEXER_PASSWORD" | /usr/share/wazuh-indexer/bin/opensearch-keystore add -x 'bootstrap.password')
+if [[ -n "$INDEXER_PASSWORD" ]]; then
+    [[ -f /usr/share/wazuh-indexer/opensearch.keystore ]] || (run_as_other_user_if_needed opensearch-keystore create)
+    if ! (run_as_other_user_if_needed opensearch-keystore has-passwd --silent) ; then
+      # keystore is unencrypted
+      if ! (run_as_other_user_if_needed opensearch-keystore list | grep -q '^bootstrap.password$'); then
+        (run_as_other_user_if_needed echo "$INDEXER_PASSWORD" | opensearch-keystore add -x 'bootstrap.password')
       fi
-    fi
     else
-      # Keystore requires a password
+      # keystore requires password
       if ! (run_as_other_user_if_needed echo "$KEYSTORE_PASSWORD" \
-          | /usr/share/wazuh-indexer/bin/opensearch-keystore list | grep -q '^bootstrap.password$') ; then
+          | opensearch-keystore list | grep -q '^bootstrap.password$') ; then
         COMMANDS="$(printf "%s\n%s" "$KEYSTORE_PASSWORD" "$INDEXER_PASSWORD")"
-        (run_as_other_user_if_needed echo "$COMMANDS" | /usr/share/wazuh-indexer/bin/opensearch-keystore add -x 'bootstrap.password')
+        (run_as_other_user_if_needed echo "$COMMANDS" | opensearch-keystore add -x 'bootstrap.password')
       fi
     fi
   fi
@@ -83,12 +83,12 @@ if [[ -n "$AZURE_ACCOUNT_NAME" || -n "$AZURE_ACCOUNT_KEY" || -n "$AZURE_ENDPOINT
         azure.client.default.key --stdin --force
   fi
 
-  # If you need a custom endpoint suffix (e.g., for GovCloud):
-  if [[ -n "$AZURE_ENDPOINT_SUFFIX" ]]; then
-    echo "$AZURE_ENDPOINT_SUFFIX" | \
-      run_as_other_user_if_needed /usr/share/wazuh-indexer/bin/opensearch-keystore add \
-        azure.client.default.endpoint_suffix --stdin --force
-  fi
+  # # If you need a custom endpoint suffix (e.g., for GovCloud):
+  # if [[ -n "$AZURE_ENDPOINT_SUFFIX" ]]; then
+  #   echo "$AZURE_ENDPOINT_SUFFIX" | \
+  #     run_as_other_user_if_needed /usr/share/wazuh-indexer/bin/opensearch-keystore add \
+  #       azure.client.default.endpoint_suffix --stdin --force
+  # fi
 fi
 
 # ------------------------------------------------------------------------------
@@ -108,6 +108,12 @@ fi
 #   nohup /securityadmin.sh &
 #   touch "/var/lib/wazuh-indexer/.flag"
 # fi
+
+# Substitute environment variables in opensearch.yml
+if [ -f "${OPENSEARCH_PATH_CONF}/opensearch.yml" ]; then
+  cp "${OPENSEARCH_PATH_CONF}/opensearch.yml" "${OPENSEARCH_PATH_CONF}/opensearch.yml.template"
+  envsubst < "${OPENSEARCH_PATH_CONF}/opensearch.yml.template" > "${OPENSEARCH_PATH_CONF}/opensearch.yml"
+fi
 
 # ------------------------------------------------------------------------------
 # 5. Finally, start OpenSearch as the wazuh-indexer user
